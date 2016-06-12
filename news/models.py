b867f150837b2f7fb4e35542c0806ca877e5c3b3
Andrew Tsobenko <mrmrmrtest@gmail.com>
2016-06-12 21:52:28 +0300

    News application fixed

--- news/models.py
+++ news/models.py
@@ -5,3 +5,6 @@ class Article(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
     date_published = models.DateTimeField()
+
+    def __str(self):
+        return self.title
--- news/views.py
+++ news/views.py
@@ -1,13 +1,14 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Article
 from django.views import generic
 
 
-class ArticlesView(generic.ListView):
+class ArticlesView(LoginRequiredMixin, generic.ListView):
     paginate_by = 5
     model = Article
     template_name = 'articles/index.html'
 
 
-class ArticleView(generic.DetailView):
+class ArticleView(LoginRequiredMixin, generic.DetailView):
     model = Article
     template_name = 'articles/article.html'
